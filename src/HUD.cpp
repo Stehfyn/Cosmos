7045e1030cf8dd45b69438c3774a1ef46e88f7e3
Stephen Foster <stephenfostercpp@gmail.com>
2020-12-11 15:53:30 -0800

    Rough prototyping for HUD, need to clean up and finalize HUD offsets and string formatting

--- src/HUD.cpp
+++ src/HUD.cpp
@@ -1,1 +1,49 @@
 #include "HUD.h"
+void::HUD::displayScore(int score, Buffer buffer)
+{
+	int xOffset{ 39 };
+	int yOffset{ 1 };
+	scoreString = std::to_string(score);
+	int scoreLength = scoreString.length();
+	
+	if (scoreLength == 3 || scoreLength == 4)
+		xOffset -= 1;
+	if (scoreLength == 5)
+		xOffset -= 2;
+	
+	//scoreArray = new wchar_t[scoreString.length()];
+	for(int i = 0; i < scoreLength; i++)
+		buffer.edit(i + xOffset, yOffset, scoreString[i]);
+}
+void::HUD::displayEnemies(int remaining, Buffer buffer)
+{
+	int xOffset{ 68 };
+	int yOffset{ 1 };
+
+	currRemainString = std::to_string(remaining);
+
+	int currLength = currRemainString.length(), lastLength = lastRemainString.length();
+
+	if (currLength < lastLength)
+		buffer.edit(xOffset + 1, yOffset, L' ');
+	
+	int remainLength = currRemainString.length();
+
+	std::wstring enemyString = L"Enemies: ";
+	for (int i = 0; i < enemyString.length(); i++)
+		buffer.edit(59 + i, 1, enemyString[i]);
+
+	for (int i = 0; i < remainLength; i++)
+		buffer.edit(i + xOffset, yOffset, currRemainString[i]);
+
+	lastRemainString = std::to_string(remaining);
+}
+void::HUD::displayAccuracy(double accuracy, Buffer buffer)
+{
+	int xOffset{};
+	int yOffset{};
+
+	currAccString = std::to_string(accuracy);
+	for (int i = 0; i < currAccString.length(); i++)
+		buffer.edit(i + xOffset, yOffset, currAccString[i]);
+}
--- src/HUD.h
+++ src/HUD.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <string>
+#include "Buffer.h"
+#include "Game.h"
 class HUD
 {
-};
+public:
+	void displayScore(int, Buffer);
+	void displayEnemies(int, Buffer);
+	void displayAccuracy(double, Buffer);
+private:
+	std::string currAccString{};
+	std::string lastAccString{};
 
+	std::string scoreString{};
+
+	std::string currRemainString{};
+	std::string lastRemainString{};
+
+	wchar_t* scoreArray{};
+};
