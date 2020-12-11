8f50607a4cad0aae400730c0708ca4ecca2b5131
Stephen Foster <stephenfostercpp@gmail.com>
2020-12-11 15:55:28 -0800

    Implemented new functionality into our main() prototype. Encapsulation and segmenting will follow once all functions are built and working properly.

--- src/Cosmos.cpp
+++ src/Cosmos.cpp
@@ -5,6 +5,7 @@
 #include "Buffer.h"
 #include "Ambience.h"
 #include "Game.h"
+#include "HUD.h"
 
 #define TICK_RATE 16.67ms //60 fps
 using namespace std;
@@ -17,46 +18,62 @@ int main()
 	Buffer buffer(ScreenWidth, ScreenHeight);
 	Ambience ambience{};
 	Game game{};
-
+	HUD hud{};
+	//buffer.setWindow();
 	int tickCounter{};
 	
 	//game loop
 	bool run = true;
 	while (run)
 	{
-		//take clock time
+     		//take clock time
 		//===============
 		auto start = std::chrono::high_resolution_clock::now();
-
 		//get user input
-		game.getInput(buffer);
-		
+		game.getInput(buffer, tickCounter);
+		//game.check(buffer);
 		//--input buffer
 
 		//get game logic input
 		//--game logic buffer
 		//compare and calculate next frame
-		
-		if (tickCounter % 1 == 0)
+		if (tickCounter % 2 == 0)
 		{
 			ambience.addStar();
 			ambience.advanceStars(buffer);
 		}
+		if (tickCounter % 1 == 0)
+		{
+			game.advanceLaser(buffer);
+		}
 		ambience.printStars(buffer);
-
-
+		if (tickCounter % 30 == 0)
+		{
+			game.advanceEnemy(buffer);
+		}
+		if (tickCounter % 60 == 0)
+		{
+			game.addEnemy(rand() % 80, 0, 1, L'?');
+		}
+		game.check(buffer);
 		//===============
 		//take clock time
 		//fix framerate based on elapsed
 		//debug - draw player
+
+		game.printEnemy(buffer);
+		game.printLaser(buffer);
 		game.printPlayer(buffer);
 		//update frame with next frame
+		game.addScore(1);
+		hud.displayScore(game.getScore(), buffer);
+		hud.displayEnemies(game.getRemaining(), buffer);
+		hud.displayAccuracy(game.calcAccuracy(), buffer);
 		buffer.update();
 		tickCounter++;
 		auto end = chrono::high_resolution_clock::now();
 		auto elapsed = chrono::duration_cast<chrono::microseconds>(start - end);
-		this_thread::sleep_for(TICK_RATE - elapsed);
+		this_thread::sleep_for((TICK_RATE - elapsed)/2);
 	}
-	
 	return 0;
 }
